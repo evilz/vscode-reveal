5070807a1358e93c5295affefb788b3001774fff
evilz <evilznet@gmail.com>
2017-02-18 11:16:43 +0100

    Start travis

--- test/Helpers.test.ts
+++ test/Helpers.test.ts
@@ -1,9 +1,30 @@
-import * as assert from "assert";
-import * as chai from "chai";
+import * as vscode from 'vscode';
+import { should } from 'chai';
 import { Helpers } from "../src/Helpers";
 import { Configuration } from "../src//Configuration";
 
-chai.should();
+should();
+let slidesWithFront = `---
+theme : "white"
+transition: "zoom"
+highlightTheme: "darkula"
+---
+
+## Slide 1.1
+
+
+\`\`\`js
+var a = 1;
+\`\`\`
+
+---
+
+## Slide 1.2
+
+---
+
+## Slide 2`;
+
 suite("Helpers Tests", () => {
 
 
@@ -44,38 +65,5 @@ suite("Helpers Tests", () => {
         let count = helpers.getInnerSlideCount(content);
         count.should.be.equal(2);
     });
-
-    // Defines a Mocha unit test
-    test("Shoud count slides when having front header and source code", () => {
-
-        let configuration = new Configuration();
-        let helpers = new Helpers(configuration);
-
-        let content = `---
-theme : "white"
-transition: "zoom"
-highlightTheme: "darkula"
----
-
-## Slide 1.1
-
-
-\`\`\`js
-var a = 1;
-\`\`\`
-
----
-
-## Slide 1.2
-
----
-
-## Slide 2`;
-
-        let count = helpers.getSlideCount(content);
-        count.should.be.equal(3);
-    });
-
-
 
 });
