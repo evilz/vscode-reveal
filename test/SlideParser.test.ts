d4768adfc40a05d3a90edb6972cf144410ab5e02
Vincent B <evilznet@gmail.com>
2019-01-14 22:12:13 +0100

    fix

--- test/SlideParser.test.ts
+++ test/SlideParser.test.ts
@@ -3,41 +3,41 @@ import { should } from 'chai'
 import * as fs from 'fs'
 import * as matter from 'gray-matter'
 import * as path from 'path'
+import { defaultConfiguration, getDocumentOptions } from '../src/Configuration'
 // import * as vscode from 'vscode'
 import { countLines, parseSlides } from '../src/SlideParser'
-import { IExtensionOptions, Configuration, IDocumentOptions, defaultConfiguration ,getDocumentOptions } from '../src/Configuration';
 
 // const sampleFile = fs.readFileSync('../sample.md').toString()
 
 const config = getDocumentOptions(defaultConfiguration)
 
-config.notesSeparator= 'note:'
-config.separator= '^[\r\n?|\n]---[\r\n?|\n]$',
-config.verticalSeparator= '^[\r\n?|\n]--[\r\n?|\n]$'
+config.notesSeparator = 'note:'
+config.separator = '^[\r\n?|\n]---[\r\n?|\n]$'
+config.verticalSeparator = '^[\r\n?|\n]--[\r\n?|\n]$'
 
 should()
-describe('Slide parser tests', function () {
-  let slideContent = ''
+describe('Slide parser tests', () => {
+    let slideContent = ''
 
-  before(function () {
-    const currentPath = path.dirname(this!.test!.parent!.file!)
-    const samplePath = path.join(currentPath, '../../sample.md')
-    const data = fs.readFileSync(samplePath)
-    slideContent = matter(data.toString()).content
-  })
-
-  it('Shoud count simple slides', function () {
-    const slides = parseSlides(slideContent, config)
-    slides.should.has.lengthOf(26)
-  })
-
-  it('First slide should have correct number of lines', function () {
-    const slides = parseSlides(slideContent, config)
-
-    const expectedLineCount = [7, 6]
-
-    expectedLineCount.forEach((expected, i) => {
-      countLines(slides[i].text).should.equal(expected)
+    before(function () {
+        const currentPath = path.dirname(this!.test!.parent!.file!)
+        const samplePath = path.join(currentPath, '../../sample.md')
+        const data = fs.readFileSync(samplePath)
+        slideContent = matter(data.toString()).content
     })
-  })
+
+    it('Shoud count simple slides', function () {
+        const slides = parseSlides(slideContent, config)
+        slides.should.has.lengthOf(26)
+    })
+
+    it('First slide should have correct number of lines', function () {
+        const slides = parseSlides(slideContent, config)
+
+        const expectedLineCount = [7, 6]
+
+        expectedLineCount.forEach((expected, i) => {
+            countLines(slides[i].text).should.equal(expected)
+        })
+    })
 })
