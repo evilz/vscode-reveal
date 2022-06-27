2fd1d6f75bd05ef8b661b70c63ac75ad8765fd32
evilz <evilznet@gmail.com>
2022-06-27 21:52:46 +0200

    Express 5 and split EJS

--- src/RevealServer.ts
+++ src/RevealServer.ts
@@ -1,9 +1,11 @@
 import * as http from 'http'
-import Koa from 'koa'
-import cors from '@koa/cors'
-import render from 'koa-ejs'
-import koalogger from 'koa-logger'
-import serve from 'koa-static-server'
+
+import express from 'express'
+import { config, engine } from 'express-edge'
+import cors from 'cors'
+import morgan from 'morgan'
+import compression from 'compression'
+
 import * as path from 'path'
 
 import markdownit from './Markdown-it'
@@ -14,7 +16,10 @@ import { RevealContext } from './RevealContext'
 
 /** Http server to serve reveal presentation */
 export class RevealServer extends Disposable {
-  public readonly app = new Koa()
+
+  //public readonly app = new Koa()
+  public readonly app = express()
+
   private server: http.Server | null = null
   private readonly host = 'localhost'
 
@@ -73,60 +78,107 @@ export class RevealServer extends Disposable {
     const app = this.app
     const context = this.context
 
+    // Configure Edge if need to
+    //config({   cache: process.env.NODE_ENV === 'production' });
+
+    app.set('view engine', 'ejs');
+
+    //app.use(engine);
+    app.set('views', path.resolve(context.extensionPath, 'views'));
+
+    app.use(compression())
     app.use(cors())
     // LOG REQUEST
-    app.use(
-      koalogger((str) => {
-        context.logger.debug(str)
-      })
-    )
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {
+      stream: {
+        write: (str) => context.logger.info(str)
+      }
+    }))
 
     // EXPORT
-    app.use(this.exportMiddleware(exportHTML, () => context.isInExport))
-
-    // EJS VIEW engine
-    render(app, {
-      root: path.resolve(context.extensionPath, 'views'),
-      layout: 'template.4.3.1',
-      viewExt: 'ejs',
-      cache: false,
-      debug: false,
-    })
+    app.use(this.exportMiddleware(exportHTML, context.isInExport)
 
     // MAIN FILE
-    app.use(async (ctx, next) => {
-      if (ctx.path !== '/') {
-        return next()
+    app.use((req, res, next) => {
+      if (req.path !== '/') {
+        next()
       }
-
-      const htmlSlides = context.slides.map((s) => ({
-        ...s,
-        html: markdownit.render(s.text),
-        children: s.verticalChildren.map((c) => ({ ...c, html: markdownit.render(c.text) })),
-      }))
-      ctx.state = { slides: htmlSlides, ...context.configuration, rootUrl: this.uri }
-      await ctx.render('reveal')
+      else {
+        const htmlSlides = context.slides.map((s) => ({
+          ...s,
+          html: markdownit.render(s.text),
+          children: s.verticalChildren.map((c) => ({ ...c, html: markdownit.render(c.text) })),
+        }))
+        res.render('index', { slides: htmlSlides, ...context.configuration, rootUrl: this.uri })
+      }
     })
+
+
 
     // STATIC LIBS
     const libsPAth = path.join(context.extensionPath, 'libs')
-    app.use(serve({ rootDir: libsPAth, rootPath: '/libs' }))
+    app.use('/libs', express.static(libsPAth));
+    //app.use(serve({ rootDir: libsPAth, rootPath: '/libs' }))
 
     // STATIC RELATIVE TO MD FILE
     if (context.dirname) {
-      app.use(serve({ rootDir: context.dirname, rootPath: '/' }))
+      app.use('/', express.static(context.dirname));
+      // app.use(serve({ rootDir: context.dirname, rootPath: '/' }))
     }
 
     // ERROR HANDLER
-    app.on('error', (err) => {
-      context.logger.error(err)
-    })
+    app.use(function (err, req, res, next) {
+      context.logger.error(err.stack)
+      res.status(500).send(err.stack);
+    });
+
+    // app.on('error', (err) => {
+    //   context.logger.error(err)
+    // })
   }
 
 
   private readonly exportMiddleware = (exportfn: (ExportOptions) => Promise<void>, isInExport) => {
 
     const { exportPath } = this.context
+
+
+    return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+
+      if (isInExport()) {
+        const oldWrite = res.write
+        const oldEnd = res.end;
+
+        const chunks: any[] = [];
+
+        res.write = (chunk, cb) => {
+          chunks.push(chunk);
+          return oldWrite.apply(res, [chunk, cb]);
+        };
+
+        res.end = async (chunk, ...args) => {
+          if (chunk) {
+            chunks.push(chunk);
+          }
+          const body = Buffer.concat(chunks).toString('utf8');
+
+          const opts: IExportOptions = { folderPath: exportPath, url: req.originalUrl.split('?')[0], srcFilePath: null, data: body }
+
+          return exportfn(opts).then(
+            () => oldEnd.apply(res, [chunk, ...args])
+          )
+          // console.log(req.path, body);
+          // return oldEnd.apply(res, [chunk, ...args]);
+        };
+
+        next()
+
+      }
+      else {
+        next()
+      }
+
+    }
 
     return async (ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext, { path: string }>, next) => {
       await next()
