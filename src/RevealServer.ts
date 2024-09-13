25715fa5c240d9ae36b148997e9ecbc7025367cb
Swann Martinez <swann.martinez@protonmail.com>
2024-09-13 18:28:05 +0200

    fix: prevent binary file corruption during slide export (#1271)
    * Fix binary data corruption during export (related to https://github.com/evilz/vscode-reveal/issues/1029)
    
    * clean: typo
    
    ---------
    
    Co-authored-by: s.martinez <smartinez@xilam.com>

--- src/RevealServer.ts
+++ src/RevealServer.ts
@@ -165,12 +165,12 @@ export class RevealServer extends Disposable {
             chunks.push(chunk);
           }
           try {
-            let body = "";
+            let body: string | Buffer;
             if (chunks.length > 0 && typeof chunks[0] === 'string') {
-              body = body.concat(...(chunks as string[]));
+              body = "".concat(...(chunks as string[]));
             }
             else {
-              body = Buffer.concat(chunks).toString('utf8');
+              body = Buffer.concat(chunks);
             }
 
 
