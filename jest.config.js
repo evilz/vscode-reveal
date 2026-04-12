ac22e19c0729f6b1d952f2490b2248a8262092b6
Vincent B. <evilznet@gmail.com>
2026-04-12 12:30:35 +0200

    Restore toolchain versions to match lockfile for npm ci

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
